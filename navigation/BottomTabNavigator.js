a31d036c10d8c8b9ae77bbc394ffc0cb8611e98f
Rafael da Costa Fonsêca <57494016+RafaelCostaF@users.noreply.github.com>
2024-05-03 23:52:14 -0300

    Navegação entre abas
    Adicionada navegação

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,42 +1,78 @@
 // Learn more about createBottomTabNavigator:
 // https://reactnavigation.org/docs/bottom-tab-navigator
 import Ionicons from "@expo/vector-icons/Ionicons";
+
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useColorScheme } from "react-native";
 
 import Colors from "../constants/Colors";
-import TabOneScreen from "../screens/TabOneScreen";
-import TabTwoScreen from "../screens/TabTwoScreen";
+import Home from "../screens/Home";
+import Projetos from "../screens/Projetos";
+import Sobre from "../screens/Sobre";
+import Habilidades from "../screens/Habilidades";
+import Contato from "../screens/Contato";
 
 const BottomTab = createBottomTabNavigator();
+
+import IonIcon from '@reacticons/ionicons';
 
 export default function BottomTabNavigator() {
   const colorScheme = useColorScheme();
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName="Home"
       screenOptions={{ tabBarActiveTintColor: Colors[colorScheme].tint }}
     >
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
+        name="Home"
+        component={Home}
         options={{
           headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="ios-code" color={color} />
-          ),
+          // tabBarIcon: ({ color }) => (
+          //   <IonIcon name="home"/>
+          // ),
         }}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
+        name="Projetos"
+        component={Projetos}
         options={{
           headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="ios-code" color={color} />
-          ),
+          // tabBarIcon: ({ color }) => (
+          //   <TabBarIcon name="ios-code" color={color} />
+          // ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Sobre"
+        component={Sobre}
+        options={{
+          headerShown: false,
+          // tabBarIcon: ({ color }) => (
+          //   <TabBarIcon name="ios-code" color={color} />
+          // ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Habilidades"
+        component={Habilidades}
+        options={{
+          headerShown: false,
+          // tabBarIcon: ({ color }) => (
+          //   <TabBarIcon name="ios-code" color={color} />
+          // ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Contato"
+        component={Contato}
+        options={{
+          headerShown: false,
+          // tabBarIcon: ({ color }) => (
+          //   <TabBarIcon name="ios-code" color={color} />
+          // ),
         }}
       />
     </BottomTab.Navigator>
@@ -47,34 +83,4 @@ export default function BottomTabNavigator() {
 // https://icons.expo.fyi/
 function TabBarIcon(props) {
   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-}
-
-// Each tab has its own navigation stack, you can read more about this pattern here:
-// https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-const TabOneStack = createStackNavigator();
-
-function TabOneNavigator() {
-  return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
-        component={TabOneScreen}
-        options={{ headerTitle: "Tab One Title" }}
-      />
-    </TabOneStack.Navigator>
-  );
-}
-
-const TabTwoStack = createStackNavigator();
-
-function TabTwoNavigator() {
-  return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: "Tab Two Title" }}
-      />
-    </TabTwoStack.Navigator>
-  );
 }
--- screens/Projetos.js
+++ screens/Projetos.js
@@ -1,18 +1,15 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet , Text, View } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
-
-export default function TabOneScreen() {
+export default function Projetos() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Projetos</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+
     </View>
   );
 }
