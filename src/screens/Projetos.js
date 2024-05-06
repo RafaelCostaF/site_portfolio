5d17e141313cacd60288ec209a37be0a5f57d732
Rafael da Costa Fonsêca <57494016+RafaelCostaF@users.noreply.github.com>
2024-05-05 22:04:05 -0300

    Estilização

--- src/screens/Projetos.js
+++ src/screens/Projetos.js
@@ -2,20 +2,36 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Typography, Paper, Grid, Button } from '@mui/material';
 
-
-
-function ProjectCard({ title, description, link }) {
+function ProjectCard({ title, description, link, image }) {
   return (
-    <Paper elevation={3} sx={{ p: 2 }}>
-      <Typography variant="h6" gutterBottom>
-        {title}
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        {description}
-      </Typography>
-      <Button component={Link} to={link} variant="contained" size="small">
-        Ver detalhes
-      </Button>
+    <Paper elevation={3} sx={{ p: 2, position: 'relative' }}>
+      {/* Image positioned on top */}
+      <img
+        src={image}
+        alt={title}
+        style={{
+          width: '100%',
+          height: '200px', // Adjust the height as needed
+          objectFit: 'cover', // Maintain aspect ratio and cover the container
+          borderRadius: '4px 4px 0 0', // Rounded corners for the top
+          position: 'absolute', // Position on top of the card
+          top: 0, // Align to the top of the card
+          left: 0, // Align to the left of the card
+        }}
+      />
+      {/* Content */}
+      <div style={{ marginTop: '200px', padding: '16px' }}>
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {description}
+        </Typography>
+        <Button component={Link} to={link} variant="contained" size="medium">
+          
+          Ver detalhes
+        </Button>
+      </div>
     </Paper>
   );
 }
@@ -24,19 +40,22 @@ function ProjectCard({ title, description, link }) {
 export default function Projetos() {
   const projects = [
     {
-      title: 'Projeto 1',
-      description: 'Descrição do Projeto 1',
-      link: '/projeto-1',
+      title: 'Mobile - Emprestimo',
+      description: 'React Native App Emprestimo',
+      link: 'https://github.com/RafaelCostaF/React-Native-APP-Emprestimo',
+      image: "https://github.com/RafaelCostaF/React-Native-APP-Emprestimo/raw/main/img/Capture-inputs.png"
     },
     {
-      title: 'Projeto 2',
-      description: 'Descrição do Projeto 2',
-      link: '/projeto-2',
+      title: 'API - Emprestimo',
+      description: 'API para aplicativo de emprestimo',
+      link: 'https://github.com/RafaelCostaF/API-Emprestimo',
+      image:"https://github.com/RafaelCostaF/API-Emprestimo/raw/main/img/example.png"
     },
     {
       title: 'Projeto 3',
       description: 'Descrição do Projeto 3',
-      link: '/projeto-3',
+      link: 'https://github.com/RafaelCostaF/Video-Room-Server',
+      image:""
     },
   ];
 
@@ -53,6 +72,7 @@ export default function Projetos() {
               title={project.title}
               description={project.description}
               link={project.link}
+              image={project.image}
             />
           </Grid>
         ))}
--- src/screens/Sobre.js
+++ src/screens/Sobre.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Container, Typography, Paper } from '@mui/material';
+import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
+import { Margin } from '@mui/icons-material';
 
 
 function Sobre() {
@@ -7,34 +10,63 @@ function Sobre() {
     
     <Container maxWidth="md" sx={{ marginTop: '4rem' }}>
       <Paper elevation={3} sx={{ padding: '2rem' }}>
-        <Typography variant="h4" gutterBottom>
-          Sobre
-        </Typography>
-        <Typography variant="body1">
-          Bem-vindo à nossa página Sobre!
-        </Typography>
-        
-        <Typography variant="body1">
-          Aqui você pode encontrar informações sobre nossa empresa, equipe ou qualquer outra coisa que desejamos compartilhar.
-        </Typography>
+        <div style={{marginBottom: 20}}>
+          <Typography variant="h4" gutterBottom>
+            Sobre
+          </Typography>
+          <Typography variant="body1">
+            Bem-vindo à nossa página Sobre!
+          </Typography>
+          
+          <Typography variant="body1">
+            Aqui você pode encontrar informações sobre nossa empresa, equipe ou qualquer outra coisa que desejamos compartilhar.
+          </Typography>
+        </div>
+
+
         <Typography variant="h5" gutterBottom>
           Equipe
         </Typography>
         <Typography variant="body1">
           Rafael e Rickson são membros valiosos de nossa equipe. Aqui está um breve resumo de suas informações:
         </Typography>
-        <Typography variant="h6" gutterBottom>
-          Rafael
-        </Typography>
-        <Typography variant="body1">
-        Cursando o último semestre de sistemas de informação na Universidade do Estado da Bahia(UNEB)        </Typography>
-        <Typography variant="h6" gutterBottom>
-          Rickson
-        </Typography>
-        <Typography variant="body1">
-          Cursando o último semestre de sistemas de informação na Universidade do Estado da Bahia(UNEB), pronto para novos desafios.
-        </Typography>
-      </Paper>
+        
+        
+        <Box display="flex" alignItems="center" gap={2}>
+          <img
+            src="https://media.licdn.com/dms/image/D4D03AQHYFMzYsoMFjg/profile-displayphoto-shrink_200_200/0/1699222207407?e=1720656000&v=beta&t=hErPMnymM_bGKVW9J8g1Da0KKbUp6Ic0uUjeImP6OkY"
+            alt="Profile"
+            style={{ borderRadius: '50%', width: 100, height: 100 , padding:50}}
+          />
+          <Box>
+            <Typography variant="h6" gutterBottom>
+              Rafael
+            </Typography>
+            <Typography variant="body1">
+              Cursando o último semestre de sistemas de informação na Universidade do Estado da Bahia (UNEB)
+            </Typography>
+          </Box>
+        </Box>
+
+
+        <Box display="flex" alignItems="center" gap={2}>
+          <img
+            src="https://media.licdn.com/dms/image/D5635AQGXW0U7XhrOLA/profile-framedphoto-shrink_200_200/0/1660002772504?e=1715562000&v=beta&t=hP-w0jPJmK_0FsmsEm_L0lf4Yk-y08AlABaEWkjLtNQ"
+            alt="Profile Rickson"
+            style={{ borderRadius: '50%', width: 100, height: 100 , padding:50}}
+          />
+          <Box>
+            <Typography variant="h6" gutterBottom>
+              Rickson
+            </Typography>
+            <Typography variant="body1">
+              Cursando o último semestre de sistemas de informação na Universidade do Estado da Bahia(UNEB), pronto para novos desafios.
+            </Typography>
+          </Box>
+        </Box>
+        
+
+                </Paper>
     </Container>
 
   );
